60e03ee5590a82bbdc46232123aa4c22a55e9851
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-11-22 11:47:53 +0200

    Update action.js

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,5 +1,3 @@
 import { createAction } from "@reduxjs/toolkit";
 
-export const contactSubmit = createAction("contactSubmit");
 export const changeFilter = createAction("changeFilter");
-export const contactDelete = createAction("contactDelete");
