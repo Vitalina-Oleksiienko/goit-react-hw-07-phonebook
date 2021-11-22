156ea999185aa96532d22c15407b2d5ef0eb2bfd
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-11-22 11:56:08 +0200

    Create operations.js

---
+++ src/redux/operations.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://619b5c172782760017445539.mockapi.io";
+
+export const getThunkData = createAsyncThunk(
+  "getContact",
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get("/contacts");
+      return data;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
+  }
+);
+
+export const addThunkData = createAsyncThunk(
+  "addContact",
+  async (contact, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post("/contacts", contact);
+      return data;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
+  }
+);
+
+export const deleteThunkData = createAsyncThunk(
+  "deleteContact",
+  async (contactId, { rejectWithValue }) => {
+    try {
+      const {
+        data: { id },
+      } = await axios.delete(`/contacts/${contactId}`);
+      return id;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
+  }
+);
